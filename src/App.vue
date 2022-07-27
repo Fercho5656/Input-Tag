c69dd1a0b4e26126de743013b71010db9b19ee83
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-27 00:30:43 -0500

    Page with info about input tag

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-SIUUUU
+  <h1>Input Tag</h1>
+  <ul>
+    <li>Write something and press <kbd-key>enter</kbd-key> to add a tag</li>
+    <li>Press <kbd-key>delete</kbd-key> or <kbd-key>backspace</kbd-key> key when
+      the input text is empty to delete the last tag</li>
+    <li>You can also press the '❌' to delete a tag</li>
+  </ul>
+  <input-tag />
 </template>
 
 <script setup lang="ts">
+import InputTag from './components/InputTag.vue';
+import KbdKey from './components/KbdKey.vue';
 </script>
 
 <style scoped>
+ul {
+  text-align: left;
+}
 </style>
